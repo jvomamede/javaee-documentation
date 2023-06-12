06b794c84d4edb729dad9aaf436f56ff9d461a71
João Vitor Oliveira Mamede <41207179+joaovitorhnk@users.noreply.github.com>
2023-06-12 14:48:19 -0300

    Methods HttpServletRequest

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -5,76 +5,65 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.NoResultException;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-
-import com.mysql.cj.protocol.Resultset;
-
 import model.User;
 
 public class UserDAO {
 
-	
 	public void salvar(User user) {
 		Connection connection = null;
 		PreparedStatement statement = null;
-		
+
 		try {
-			
+
 			connection = ConnectionFactory.getConnection();
-			
+
 			String sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-			
+
 			statement = connection.prepareStatement(sql);
 			statement.setString(1, user.getUsername());
 			statement.setString(2, user.getPassword());
 			statement.executeUpdate();
-			
-			
+
 		} catch (SQLException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+			e.printStackTrace();
+		} finally {
+			try {
+				if (statement != null) {
+					statement.close();
+				}
+				if (connection != null) {
+					connection.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
+
 	public User findByUsername(String username) {
-        String query = "SELECT * FROM users WHERE username = ?";
-        Connection connection = null;
-        
-        try {
-        	connection = ConnectionFactory.getConnection();
-        	PreparedStatement statement = connection.prepareStatement(query); 
-            statement.setString(1, username);
-            
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (resultSet.next()) {
-                    User user = new User();
-                    user.setId(resultSet.getLong("id"));
-                    user.setUsername(resultSet.getString("username"));
-                    user.setPassword(resultSet.getString("password"));
-                    
-                    return user;
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        
-        return null;
-    }
-	
+		String query = "SELECT * FROM users WHERE username = ?";
+		Connection connection = null;
+
+		try {
+			connection = ConnectionFactory.getConnection();
+			PreparedStatement statement = connection.prepareStatement(query);
+			statement.setString(1, username);
+
+			try (ResultSet resultSet = statement.executeQuery()) {
+				if (resultSet.next()) {
+					User user = new User();
+					user.setId(resultSet.getLong("id"));
+					user.setUsername(resultSet.getString("username"));
+					user.setPassword(resultSet.getString("password"));
+
+					return user;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 }
--- src/gettinstarterservlet/MethodRequest.java
+++ src/gettinstarterservlet/MethodRequest.java
@@ -5,25 +5,104 @@ import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/MethodRequest")
 public class MethodRequest extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
-    public MethodRequest() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public MethodRequest() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		/** obtém os nomes dos cabeçalhos **/
+		Enumeration<String> headersNames = request.getHeaderNames();
+
+		while (headersNames.hasMoreElements()) {
+
+			String headerName = headersNames.nextElement();
+
+			/** Obtem o valor do cabeçalho partindo do nome **/
+			String headerValue = request.getHeader(headerName);
+
+			this.log("Header: " + headerValue);
+		}
+
+		/** Obtem valor do parametro de requisição HTTP **/
 		String user = request.getParameter("username");
 		this.log(user);
+
+		/** Obtem parametros repetidos com valores **/
+		String[] colors = request.getParameterValues("color");
+		for (String color : colors) {
+			this.log("Cores: " + color);
+		}
+
+		/** Obtem todos os nomes de parametros **/
+		Enumeration<String> parameterNames = request.getParameterNames();
+
+		while (parameterNames.hasMoreElements()) {
+			String parameterName = parameterNames.nextElement();
+			
+			String parameterValue = request.getParameter(parameterName);
+			this.log("Parametro nome: " + parameterName);
+			
+		}
 		
+		/** Obtem os cookies contidos na requisição **/
+		Cookie[] cookies = request.getCookies();
+		
+		for(Cookie cookie: cookies) {
+			/** Obtem o nome do cookie **/
+			String name = cookie.getName();
+			this.log("Cookie: " + name);
+			
+			/** Obtem o valor do cookie **/
+			String value = cookie.getValue();
+			this.log("Cookie Valor: " + value);
+		}
+		
+		/** Obtem a sessão associada a requisição **/
+		HttpSession session = request.getSession();
+		
+		/** Armaenzar um atributo na sessão **/
+		session.setAttribute("username", "john123");
+		
+		/** Obtendo o atribuido da sessão setada **/
+		String username = (String) session.getAttribute("username");
+		this.log("Valor de sessão: " + username);
+		
+		/** invalidar a sessão **/
+		session.invalidate();
+		
+		/** Defini um atributo no escopo da requisição **/
+		String attributeName = "message";
+		String attributeValue = "Hello World";
+		request.setAttribute(attributeName, attributeValue);
+		
+		/** Obtem o atributo setado no escopo da requisição **/
+		String retrievedValue = (String) request.getAttribute(attributeName);
+		this.log("Atributo de requisição: " + retrievedValue);
+		
+		/** Obtem o valor do metodo HTTP utilizado na requisição **/
+		String method = request.getMethod();
+		this.log("Metodo HTTP: " + method);
+		
+		/** Obtem a URL da requsição **/
+		StringBuffer url = request.getRequestURL();
+		this.log("URL da requisição: "+ url);
+		
+		/** Obtem a URI da requisição **/
+		String uri = request.getRequestURI();
+		this.log("URI da requisição: " + uri);
 	}
 
 }
